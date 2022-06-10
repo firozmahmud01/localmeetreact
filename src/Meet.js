ccc7f7eb584972c6e3e3b129057c6c019cfb6aff
firozmahmud <sayodfiroz@gmail.com>
2022-06-11 01:49:10 +0600

    new update

--- src/Meet.js
+++ src/Meet.js
@@ -1,6 +1,14 @@
+import { Camera, MicOff, ScreenShare, Share, StopScreenShare, Videocam, VideocamOff } from "@mui/icons-material";
+import  Mic from "@mui/icons-material/Mic";
+import { IconButton } from "@mui/material";
+import { useState } from "react";
 import Peer from "simple-peer";
 let hostname=''
+let svideo=undefined
+let isshare=false
 let peer=undefined;
+let myvid=undefined;
+let myaudio=undefined;
 async function checkdata(init){
     let data=await fetch('http://'+hostname+(init?'/checkinit':'/checksome'))
     let res=await data.text()
@@ -47,8 +55,16 @@ async function checkreturnsignal(){
     
 }
 function VideoView(){
-    return <video style={{position:'fixed'
+    return (<div>
+        <video id="audio" hidden/>
+        <video style={{position:'fixed'
     ,height:'100%',
+    margin: 'auto',
+    position:'fixed',
+    top:'0',
+    bottom:'0',
+    left:'0',
+    right:'0',
     width:'100%'
 }} id="videoview" ref={async(e)=>{
     
@@ -63,44 +79,170 @@ function VideoView(){
             await sendpeerdata(pdata,'somethingelse')
         }
     }
-}}/>
+}}/></div>)
 }
 
+
+let screen=false;
+let one=false,two=false;
+async function initpeerwithstream(initiator,sign){
+    return new Promise(async(reso,err)=>{
+        myaudio=await navigator.mediaDevices.getUserMedia({audio: true})
+        myvid=await navigator.mediaDevices.getUserMedia({video: true})
+        myvid.getVideoTracks()[0].enabled=false;
+        myaudio.getAudioTracks()[0].enabled=false;
+        let sts=[myvid,myaudio]
+        let convalue=window.confirm('Do you want to share screen?')
+        if(convalue){
+            let scre=await navigator.mediaDevices.getDisplayMedia({video: true,audio:true})
+            isshare=true
+            setVidS(scre)
+            sts.push(scre)
+        }
+        peer=new Peer({
+            'initiator':initiator,
+            trickle:false,
+            streams:sts
+        })
+        peer.on('signal',(data)=>{
+            reso(data)
+        })
+        
+        peer.on('stream',stre=>{
+            
+            let vid=stre.getVideoTracks()
+            let audioele=document.getElementById('audio')
+            
+            if(vid.length<=0){
+                
+                one=true
+                audioele.srcObject=stre;
+                audioele.play()
+            }else{
+                two=true
+                if(!isshare){
+                setVidS(stre)
+                }
+            }
+            
+        })
+        
+        if(!initiator){
+            peer.signal(sign)
+        }
+        
+        
+    })
+}
 export default function MainView(){
     hostname=prompt('Input the ip:')
     return (
         <div>
-        
         <VideoView/>
+        <BottomPanal/>
     </div>
 )
 }
+function BottomPanal(){
+    const [mic,setMic]=useState(false)
+    const [video,setVideo]=useState(false)
+    const [sshare,setShare]=useState(false)
+    return (
+    <div style={{
+            margin:'auto',
+            width:'145px'
+        }}>
 
-let screen=false;
-let audio=false;
-async function initpeerwithstream(initiator,sign){
-    return new Promise(async(reso,err)=>{
-    let vid=await navigator.mediaDevices.getUserMedia({video: true,audio:true})
-    peer=new Peer({
-        'initiator':initiator,
-        trickle:false,
-        stream:vid
+
+        <IconButton size="large" color="primary" 
+        onClick={()=>{
+            if(mic){
+                stopAudio()
+            }else{
+                startAudio()
+            }
+            setMic(!mic)
+    
+        }}>
+        {mic?(<Mic></Mic>):(<MicOff/>)}
+        </IconButton>
+        <IconButton size="large" color="primary"
+        onClick={()=>{
+            if(video){
+                stopVideo()
+            }else{
+                startVideo()
+            }
+            setVideo(!video)
+        }}>
+        {video?(<Videocam/>):(<VideocamOff/>)}
+        </IconButton>
+    </div>
+    )
+}
+function shareScreen(setShare){
+    navigator.mediaDevices.getDisplayMedia({video:true
+    ,audio:true}).then((md)=>{
+        
+        svideo=md
+        md.getVideoTracks()[0].onended=()=>{
+            stopShare(setShare,md)
+        }
+        peer.on('data',dataa=>{
+            let data=JSON.parse(dataa)
+            if('init' in data){
+                let p=new Peer({initiator:false,trickle:false})
+                p.on('signal',sign=>{
+                    peer.send(JSON.stringify({'another':sign}))
+                })
+                p.on('stream',stre=>{
+                    setVidS(stre)
+                })
+                p.signal(data.init)
+            }else{
+                sharepeer.signal(data.another)
+            }
+        })
+        makeShare(md)
+        
+        setVidS(md)
     })
-    peer.on('signal',(data)=>{
-        reso(data)
+}
+let sharepeer=undefined
+function makeShare(stream){
+    sharepeer=new Peer({initiator:true,trickle:false,'stream':stream})
+    sharepeer.on('signal',(data)=>{
+        peer.send(JSON.stringify({init:data}))
     })
-    peer.on('stream',stre=>{
-        alert('stream found')
-        let vobj=document.getElementById('videoview')
-        vobj.srcObject=stre
-        vobj.play()
-    })
-    peer.on('connect',()=>{
-        alert('You are connected.')
-    })
-    if(!initiator){
-        peer.signal(sign)
+    
+}
+
+
+function setVidS(stream){
+    let vobj=document.getElementById('videoview')
+    vobj.srcObject=stream
+    vobj.play()
+}
+function stopShare(setShare,stream){
+    sharepeer.close()
+    let alls=stream.getTracks()
+    for(let i=0;i<alls.length;i++){
+        alls[i].stop()
     }
-
-})
+    setShare(false)
+}
+function startVideo()
+{
+    myvid.getVideoTracks()[0].enabled=true;
+    
+}
+function stopVideo(){
+    myvid.getVideoTracks()[0].enabled=false;
+    
+}
+function startAudio(){
+    myaudio.getAudioTracks()[0].enabled=true;
+}
+function stopAudio(){
+    myaudio.getAudioTracks()[0].enabled=false;
 }
