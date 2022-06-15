99952308ff5c7ecb59395fd5d03a7b50dd27f498
firozmahmud <sayodfiroz@gmail.com>
2022-06-15 11:46:30 +0600

    problem solved

--- src/Meet.js
+++ src/Meet.js
@@ -1,62 +1,63 @@
 import { Camera, MicOff, ScreenShare, Share, StopScreenShare, Videocam, VideocamOff } from "@mui/icons-material";
 import  Mic from "@mui/icons-material/Mic";
-import { IconButton } from "@mui/material";
-import { useState } from "react";
+import { Alert, IconButton } from "@mui/material";
+import { useEffect, useState } from "react";
 import Peer from "simple-peer";
+import {io} from 'socket.io-client'
+let sock=undefined;
 let hostname=''
-let svideo=undefined
-let isshare=false
-let peer=undefined;
-let myvid=undefined;
-let myaudio=undefined;
-async function checkdata(init){
-    let data=await fetch('http://'+hostname+(init?'/checkinit':'/checksome'))
-    let res=await data.text()
-    if(res=='none'){
-        return undefined
-    }else{
-        return JSON.parse(res)
-
+let myvid=undefined
+let myid=undefined;
+let myaudio=undefined
+let myshare=undefined
+let allshare={}
+let allpeer={}
+async function getHostname(){
+    hostname=prompt('Ip address:')
+    let res=''
+    try{
+    let re=await fetch('http://'+hostname+'/nettest')
+    res=await re.text()
+    }catch(e){}
+    if(res!='ok'){
+        alert('Wrong ip address')
+        await getHostname()
     }
 }
+async function initeve(){
+    myvid=await navigator.mediaDevices.getUserMedia({video:true})
+    myaudio=await navigator.mediaDevices.getUserMedia({audio:true})
+    stopAudio()
+    stopVideo()
+    await getHostname()
 
-
-async function sendpeerdata(data,type){
-    let res=await fetch('http://'+hostname+'/iamasking',{headers:
-    {
-        'Content-Type': 'application/json'
-    },
-    method:'POST',
-    body:JSON.stringify({'data':data,'type':type})
-})
-let da=res.text();
-if(da=='thankyou'){
-    return 'ok'
+    sock=io("ws://"+hostname+':3100')
+    sock.on('connect',()=>{
+        
+        sock.emit('newuserreq');
+    })
+    sock.on('setid',(id)=>{
+        myid=id;
+    })
+    sock.on('stopvideo',stopremotevideo);
+    sock.on('startvideo',startremotevideo);
+    sock.on('deleteuser',deleteremoteuser);
+    sock.on('cstopshare',stopremoteshare);
+    sock.on('startshare',startremoteshare);
+    sock.on('newuser',newuserreq)
+    sock.on('chello',addUsers)
+    sock.on('chelloback',signalfound)
+    sock.on('cstartshare',cstartshare)
+    sock.on('cwantshare',shareback)
+    sock.on('cshareback',foundshare)
 }
+function signalfound(id,sign){
+    allpeer[id].peer.signal(sign)
 }
 
-
-
-
-let ispeerset=false;
-async function checkreturnsignal(){
-    let tin=setInterval(async()=>{
-        let res=await checkdata(false)
-        if(res!=undefined){
-            if('somethingelse' in res){
-                if(!ispeerset){
-                    ispeerset=true;
-                    clearInterval(tin)
-                    peer.signal(res.somethingelse)
-                }
-            }
-        }
-    },1000)
-    
-}
 function VideoView(){
     return (<div>
-        <video id="audio" hidden/>
+        <div id='audio'></div>
         <video style={{position:'fixed'
     ,height:'100%',
     margin: 'auto',
@@ -66,78 +67,14 @@ function VideoView(){
     left:'0',
     right:'0',
     width:'100%'
-}} id="videoview" ref={async(e)=>{
-    
-    if(peer==undefined){
-        let data=await checkdata(true)
-        if(data==undefined){
-            let pdata=await initpeerwithstream(true)
-            await sendpeerdata(pdata,'init')
-            checkreturnsignal()
-        }else{
-            let pdata=await initpeerwithstream(false,data.init)
-            await sendpeerdata(pdata,'somethingelse')
-        }
-    }
-}}/></div>)
+}} id="videoview"/></div>)
 }
 
 
-let screen=false;
-let one=false,two=false;
-async function initpeerwithstream(initiator,sign){
-    return new Promise(async(reso,err)=>{
-        myaudio=await navigator.mediaDevices.getUserMedia({audio: true})
-        myvid=await navigator.mediaDevices.getUserMedia({video: true})
-        myvid.getVideoTracks()[0].enabled=false;
-        myaudio.getAudioTracks()[0].enabled=false;
-        let sts=[myvid,myaudio]
-        let convalue=window.confirm('Do you want to share screen?')
-        if(convalue){
-            let scre=await navigator.mediaDevices.getDisplayMedia({video: true,audio:true})
-            isshare=true
-            let ele=document.getElementById('videoview')
-            ele.muted=true;
-            setVidS(scre)
-            sts.push(scre)
-        }
-        peer=new Peer({
-            'initiator':initiator,
-            trickle:false,
-            streams:sts
-        })
-        peer.on('signal',(data)=>{
-            reso(data)
-        })
-        
-        peer.on('stream',stre=>{
-            
-            let vid=stre.getVideoTracks()
-            let audioele=document.getElementById('audio')
-            
-            if(vid.length<=0){
-                
-                one=true
-                audioele.srcObject=stre;
-                audioele.play()
-            }else{
-                two=true
-                if(!isshare){
-                setVidS(stre)
-                }
-            }
-            
-        })
-        
-        if(!initiator){
-            peer.signal(sign)
-        }
-        
-        
-    })
-}
 export default function MainView(){
-    hostname=prompt('Input the ip:')
+    useEffect(()=>{
+        initeve()
+    })
     return (
         <div>
         <VideoView/>
@@ -149,6 +86,10 @@ function BottomPanal(){
     const [mic,setMic]=useState(false)
     const [video,setVideo]=useState(false)
     const [sshare,setShare]=useState(false)
+
+
+
+
     return (
     <div style={{
             margin:'auto',
@@ -179,72 +120,204 @@ function BottomPanal(){
         }}>
         {video?(<Videocam/>):(<VideocamOff/>)}
         </IconButton>
+        <IconButton size="large" color="primary"
+        onClick={()=>{
+            if(video){
+                stopShare(setShare)
+            }else{
+                startShare(setShare)
+            }
+            
+        }}>
+        {sshare?(<ScreenShare/>):(<StopScreenShare/>)}
+        </IconButton>
     </div>
     )
 }
-function shareScreen(setShare){
-    navigator.mediaDevices.getDisplayMedia({video:true
-    ,audio:true}).then((md)=>{
-        
-        svideo=md
-        md.getVideoTracks()[0].onended=()=>{
-            stopShare(setShare,md)
-        }
-        peer.on('data',dataa=>{
-            let data=JSON.parse(dataa)
-            if('init' in data){
-                let p=new Peer({initiator:false,trickle:false})
-                p.on('signal',sign=>{
-                    peer.send(JSON.stringify({'another':sign}))
-                })
-                p.on('stream',stre=>{
-                    setVidS(stre)
-                })
-                p.signal(data.init)
-            }else{
-                sharepeer.signal(data.another)
-            }
-        })
-        makeShare(md)
-        
-        setVidS(md)
-    })
-}
-let sharepeer=undefined
-function makeShare(stream){
-    sharepeer=new Peer({initiator:true,trickle:false,'stream':stream})
-    sharepeer.on('signal',(data)=>{
-        peer.send(JSON.stringify({init:data}))
-    })
-    
-}
 
-
-function setVidS(stream){
-    let vobj=document.getElementById('videoview')
-    vobj.srcObject=stream
-    vobj.play()
-}
-function stopShare(setShare,stream){
-    sharepeer.close()
-    let alls=stream.getTracks()
-    for(let i=0;i<alls.length;i++){
-        alls[i].stop()
+function stopShare(setShare){
+    let keys=Object.keys(allshare)
+    for(let ky in keys){
+        try{
+        allshare[ky].destroy();
+        }catch(e){}
+        delete allshare[ky]
     }
+    allshare={}
+    sock.emit('stopshare')
     setShare(false)
+
 }
+
+async function startShare(setShare){
+    myshare=await navigator.mediaDevices.getDisplayMedia({video:true,audio:true})
+    myshare.getVideoTracks()[0].onended=()=>{
+        stopShare(setShare)
+    }
+    let ele=document.getElementById('videoview')
+    ele.muted=true
+    ele.srcObject=myshare;
+    ele.play();
+    sock.emit("startshare")
+    setShare(true)
+}
+function newusermsg(){
+    alert('Someone joined')
+}
+function addUsers(id,signal){
+    if(id==myid)return ;
+    let p=new Peer({initiator:false,trickle:false,streams:[myaudio,myvid]})
+    allpeer[id]={'peer':p};
+    p.on('signal',si=>{
+        sock.emit('helloback',id,si)
+    })
+    p.on('stream',(st)=>{
+        let coun=st.getVideoTracks()
+        if(coun.length<=0){
+            let vid=document.createElement('video')
+            vid.srcObject=st;
+            vid.hidden=true;
+            vid.id=id
+            document.getElementById('audio').appendChild(vid)
+            vid.play()
+        }else{
+            allpeer[id].video=st;
+        }
+    })
+    p.signal(signal)
+    newusermsg()
+}
+
 function startVideo()
 {
+    try{
     myvid.getVideoTracks()[0].enabled=true;
-    
+    sock.emit('startvideo')
+    }catch(e){
+
+    }
 }
 function stopVideo(){
-    myvid.getVideoTracks()[0].enabled=false;
-    
+    try{
+        myvid.getVideoTracks()[0].enabled=false;
+        sock.emit('stopvideo')
+    }catch(e){}
 }
 function startAudio(){
+    try{
     myaudio.getAudioTracks()[0].enabled=true;
+    }catch(e){}
 }
 function stopAudio(){
+    try{
     myaudio.getAudioTracks()[0].enabled=false;
+    }catch(e){}
+}
+
+function startremotevideo(id){
+    if(id==myid)return ;
+    if(!(id in allpeer))return ;
+    let ele=document.getElementById('videoview');
+
+    ele.srcObject=allpeer[id].video;
+    ele.play();
+}
+function stopremotevideo(id){
+    if(id==myid)return ;
+
+}
+function startremoteshare(id){
+    if(id==myid)return ;
+
+}
+function stopremoteshare(id){
+    if(id==myid)return ;
+    let keys=Object.keys(allshare)
+    for(let ky in keys){
+        try{
+        allshare[ky].destroy();
+        }catch(e){}
+        delete allshare[ky]
+    }
+    allshare={}
+}
+function deleteremoteuser(id){
+    if(id==myid)return ;
+    
+    if(id in allpeer){
+        delete allpeer[id]
+        try{
+            allshare[id].peer.destroy();
+        }catch(e){}
+        delete allshare[id]
+        document.getElementById(id).remove();
+        alert('Someone leaved')
+    }
+}
+function newuserreq(id){
+    if(id==myid)return ;
+    let myp=new Peer({initiator:true,trickle:false,streams:[myaudio,myvid]})
+    allpeer[id]={'peer':myp};
+    myp.on('signal',sig=>{
+        sock.emit('hello',id,sig)
+    })
+    myp.on('stream',st=>{
+        let coun=st.getVideoTracks()
+        if(coun.length<=0){
+            let vid=document.createElement('video')
+            vid.srcObject=st;
+            vid.hidden=true;
+            vid.id=id
+            document.getElementById('audio').appendChild(vid)
+            vid.play()
+        }else{
+            allpeer[id].video=st;
+        }
+    })
+    
+    newusermsg()
+}
+
+function cstartshare(id){
+    if(id==myid)return ;
+    console.log('someone want to share')
+    let myp=new Peer({initiator:true,trickle:false})
+    allshare[id]={'peer':myp};
+    
+    myp.on('signal',(sig)=>{
+        
+        sock.emit('wantshare',id,sig)
+        
+    })
+    myp.on('stream',st=>{
+        allshare[id].video=st;
+        let ele=document.getElementById('videoview')
+        ele.srcObject=st;
+        ele.muted=false;
+        ele.play()
+    })
+
+}
+function foundshare(id,signal){
+    allshare[id].peer.signal(signal);
+}
+function shareback(id,signal){
+    if(id==myid)return ;
+    if(id in allshare){
+        allshare[id].peer.signal(signal);
+    }else{
+    let myp=new Peer({initiator:false,trickle:false,stream:myshare})
+    allshare[id]={'peer':myp};
+    myp.on('signal',sig=>{
+        sock.emit('shareback',id,sig)
+    })
+    myp.on('stream',st=>{
+        allshare[id].video=st;
+        let ele=document.getElementById('videoview')
+        ele.srcObject=st;
+        ele.muted=false;
+        ele.play()
+    })
+    myp.signal(signal)
+    }
 }
